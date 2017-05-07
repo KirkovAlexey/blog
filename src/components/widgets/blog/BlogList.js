1198004005320dea938ec230d9fc35f34de3f3d7
KirkovAlexey <kirkov.alexey@gmail.com>
2017-05-07 10:38:24 +0400

    add PieChart to BlogList

--- src/components/widgets/blog/BlogList.js
+++ src/components/widgets/blog/BlogList.js
@@ -3,6 +3,7 @@ import React, { PropTypes } from 'react';
 import { map } from 'lodash/collection';
 
 import BlogItem from './BlogItem';
+import PieChart from './PieChart';
 
 import { postsPath } from 'helpers/routes';
 
@@ -10,21 +11,29 @@ const BlogList = ({ elements, handleLikeClick }) => (
   React.createElement(
     'div',
     {},
-    map(
-      elements,
-      (element) => (
-        React.createElement(
-          BlogItem,
-          Object.assign(
-            { key: element.id },
-            { element: Object.assign(
-                        element,
-                        { link: postsPath(element.id) }
-                       ) },
-            { handleLikeClick }
+    React.createElement(
+      'div',
+      {},
+      map(
+        elements,
+        (element) => (
+          React.createElement(
+            BlogItem,
+            Object.assign(
+              { key: element.id },
+              { element: Object.assign(
+                          element,
+                          { link: postsPath(element.id) }
+                         ) },
+              { handleLikeClick }
+            )
           )
         )
       )
+    ),
+    React.createElement(
+      PieChart,
+      { columns: map(elements, (element) => ([element.description.text, element.meta.count])) }
     )
   )
 );
