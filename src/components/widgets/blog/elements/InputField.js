cfbac62a1b5eaa8e963724d2faa64933a9d05654
KirkovAlexey <kirkov.alexey@gmail.com>
2017-05-08 18:01:09 +0400

    Changing the processing logic

--- src/components/widgets/blog/elements/InputField.js
+++ src/components/widgets/blog/elements/InputField.js
@@ -2,19 +2,33 @@ import React, { PropTypes } from 'react';
 
 import { Input } from 'semantic-ui-react';
 
+import { bind } from 'lodash';
+
 class InputField extends React.Component {
   constructor(props) {
     super(props);
     this.placeholder = props.placeholder;
-    this.handleInputField = props.handleInputField;
+    this.processOnKeyDown = bind(this.processOnKeyDown, this);
+  }
+
+  processOnKeyDown(event) {
+    if (event.keyCode === 13) {
+      event.preventDefault();
+      const textSeacrh = event.currentTarget.value;
+      let path;
+      if (textSeacrh)
+        path = `/result?q=${textSeacrh}`;
+      else
+        path = '/';
+      return this.props.handleInputField(path);
+    }
   }
 
   render() {
     const placeholder = this.placeholder;
-    const handleInputField = this.handleInputField;
     return React.createElement(
       Input,
-      { name: 'q', placeholder, onKeyDown: handleInputField }
+      { name: 'q', placeholder, onKeyDown: this.processOnKeyDown }
     );
   }
 }
--- src/containers/PostsContainer.js
+++ src/containers/PostsContainer.js
@@ -13,15 +13,8 @@ const stateToProps = (state) => ({
     Object.assign(element, { link: postsPath(element.id) }))),
   isFetching: state.posts.isFetching,
   error: state.posts.error,
-  handleInputField: (event) => {
-    if (event.keyCode === 13) {
-      event.preventDefault();
-      const textSeacrh = event.currentTarget.value;
-      if (textSeacrh)
-        browserHistory.push(`/result?q=${textSeacrh}`);
-      else
-        browserHistory.push('/');
-    }
+  handleInputField: (path) => {
+    browserHistory.push(path);
   }
 });
 
