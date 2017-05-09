d92a3809f3c59ad9c446106b31440e162c2dfeb1
KirkovAlexey <kirkov.alexey@gmail.com>
2017-05-09 15:39:36 +0400

    fix search feature

--- src/components/views/Index.js
+++ src/components/views/Index.js
@@ -4,13 +4,13 @@ import Helmet from 'react-helmet';
 
 import BlogList from 'components/widgets/blog/BlogList';
 
-const Index = ({ elements }) => (
+const Index = ({ elements, handleInputField }) => (
   React.createElement(
     'div',
     {},
     React.createElement(
       BlogList,
-      { elements }
+      { elements, handleInputField }
     ),
     React.createElement(
       Helmet,
@@ -22,7 +22,8 @@ const Index = ({ elements }) => (
 );
 
 Index.propTypes = {
-  elements: BlogList.propTypes.elements
+  elements: BlogList.propTypes.elements,
+  handleInputField: BlogList.propTypes.handleInputField
 };
 
 export default Index;
